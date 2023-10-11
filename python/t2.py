c832611113e76456210fa5b8b992790dec1ac0d7
zahraahmadabady <147085921+zahraahmadabady@users.noreply.github.com>
2023-10-11 12:34:34 +0330

    Add files via upload

---
+++ python/t2.py
@@ -0,0 +1,19 @@
+def dict_1():
+    user={"Zahra":123,"mina":7654,"Ali":0000}
+    user.pop("Ali")
+    print(user)
+dict_1()
+
+def list_1():
+    friends=["zahra","mina","Ali"]
+    friends.pop(2)
+    print(friends)  
+list_1()
+
+
+def tuple_1():
+    color=("red","green","blue","gray")
+    print(color)
+tuple_1()
+#حذف یک عنصر امکان پذیر نیست
+
